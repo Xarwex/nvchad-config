7a3eebc49fdc5d60a49b7ce029ebeb94d433b1a4
Aleksander Kleinszmidt <60325364+Xarwex@users.noreply.github.com>
2023-06-10 13:32:00 +0100

    add auto-session

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -2,5 +2,5 @@
  local M = {}
  M.ui = {theme = 'catppuccin'}
  M.plugins = 'custom.plugins'
- M.mappings = require "custom.mappings"-- place this in one of your configuration file(s)
+ M.mappings = require "custom.mappings"
  return M
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -81,6 +81,28 @@ local plugins = {
       require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
       require("core.utils").load_mappings("hop")
     end
+  },
+  {
+    "rmagatti/auto-session",
+    lazy = false,
+    config = function()
+      require("auto-session").setup {
+        log_level = "error",
+        auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
+      }
+      -- Workaround for restoring nvim-tree properly
+      vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+        pattern = 'NvimTree*',
+        callback = function()
+          local api = require('nvim-tree.api')
+          local view = require('nvim-tree.view')
+
+          if not view.is_visible() then
+            api.tree.open()
+          end
+        end,
+      })
+    end
   }
 }
 return plugins
