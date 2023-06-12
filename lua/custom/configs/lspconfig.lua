8c649736d2c6a12b245bf4df5fa49bbab3b48db1
Aleksander <aleksander.kleinszmidt@smarkets.com>
2023-06-12 15:54:04 +0100

    python

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -1,12 +1,11 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
+local lspconfig = require("lspconfig")
+local util = require "lspconfig/util"
 
-local options = {
-  server = {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
-}
-
-return options
+lspconfig.pyright.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"python"},
+})
