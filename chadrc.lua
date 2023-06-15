4e9f36c660f1ad858bdb884f942f500241b9b6c0
Aleksander <aleksander.kleinszmidt@smarkets.com>
2023-06-15 18:10:59 +0100

    remove path when extension is empty

--- chadrc.lua
+++ chadrc.lua
@@ -17,7 +17,7 @@
       -- this is just default table of statusline modules
       return {
         fileInfo = function()
-          local prefix = "%#St_file_info#" .. vim.fn.expand "%:h" .. "%#St_file_sep#"
+          local prefix = (vim.fn.expand "%:e" == "" and "") or ("%#St_file_info#" .. vim.fn.expand "%:h" .. "%#St_file_sep#")
           return prefix .. st_modules.fileInfo()
           -- or just return "" to hide this module
         end,
