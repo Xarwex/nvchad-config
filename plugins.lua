63e4ae1d5b6aeaa43ae72da6d482b751741f4839
Aleksander <aleksander.kleinszmidt@smarkets.com>
2023-07-19 09:56:33 +0100

    num of pickers

--- plugins.lua
+++ plugins.lua
@@ -82,7 +82,7 @@ local plugins = {
     "nvim-telescope/telescope.nvim",
     opts = function (_, opts)
       opts.defaults.cache_picker = {
-        num_pickers = 15
+        num_pickers = 30
       }
       return table.insert(opts.extensions_list, "live_grep_args")
     end
